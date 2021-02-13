c36e8b35f62b4d9b8cb9806592a8321cf30ec75d
Oluwatobi <tboydv1@gmail.com>
2021-02-14 00:46:00 +0100

    Service discory setup using Eureka

---
+++ inventory-service/src/main/java/com/bookstore/inventory/InventoryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.bookstore.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
